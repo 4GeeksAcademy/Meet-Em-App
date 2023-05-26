74cfb4493d519b1655e8ef0f438ce10025360cdb
Alexander T.C <82457422+FluxFeint@users.noreply.github.com>
2023-05-26 10:28:53 +0000

    Quitado el moñeco y preparado el main

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,5 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => {
@@ -9,9 +8,6 @@ export const Home = () => {
 	return (
 		<div className="text-center mt-5">
 			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
 			<div className="alert alert-info">
 				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
 			</div>
