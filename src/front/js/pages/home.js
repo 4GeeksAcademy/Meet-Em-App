a9c9cb174fc15207b8161801f5499f471814d0a0
Alexander T.C <82457422+FluxFeint@users.noreply.github.com>
2023-05-29 16:19:50 +0000

    Landing casi terminada con Navbar, footer, jumbotron, cards y footer

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,22 +1,18 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import "../../styles/home.css";
+import "../../styles/home.css"
+import NavBarLandingPage from "../component/NavBarLandingPage";
+import Jumbotron from "../component/Jumbotron.js";
+import LandingCards from "../component/LandingCards";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://start.4geeksacademy.com/starters/react-flask">
-					Read documentation
-				</a>
-			</p>
-		</div>
+        <div>
+            <NavBarLandingPage />
+            <Jumbotron />
+            <LandingCards/>
+        </div>
 	);
 };
