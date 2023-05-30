4a74308421d123ff3a6034c473f13af1c5d60900
Juan Penalver <122556197+JuanPenalver11@users.noreply.github.com>
2023-05-30 10:36:00 +0000

    sign-up-form-final

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../../styles/home.css";
-import { SignUpForm } from "../component/signUpForm";
+
 
 
 
@@ -8,7 +8,7 @@ export const Home = () => {
 
     return (
         <>
-            <SignUpForm/>
+            
         </>
 
     )
