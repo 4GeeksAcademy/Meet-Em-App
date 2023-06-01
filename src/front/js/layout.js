a05015dc5cc264b21bba46931872a68304b7ca28
crhistian2296 <garcia.crhistian@correounivalle.edu.co>
2023-06-01 21:52:12 +0200

    router hecho a medida

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,9 +3,18 @@ import { Navigate, Route, BrowserRouter as Router, Routes } from 'react-router-d
 import { BackendURL } from './component/backendURL';
 import { PrivateRoutes } from './component/privateRoutes';
 import { PublicRoutes } from './component/publicRoutes';
-import Home from './pages/home';
-import injectContext from './store/appContext';
+import Aboutus from './pages/aboutus';
+import Chat from './pages/chat';
+import CreateEvents from './pages/createEvents';
+import EditProfile from './pages/editProfile';
+import Events from './pages/events';
+import { Landing } from './pages/landing';
 import Login from './pages/login';
+import MyEvents from './pages/myEvents';
+import Profile from './pages/profile';
+import Search from './pages/search';
+import { SignUp } from './pages/signUp';
+import injectContext from './store/appContext';
 
 //create your first component
 const Layout = () => {
@@ -21,13 +30,21 @@ const Layout = () => {
             <Router basename={basename}>
                 <div className='main'>
                     <Routes>
-                        <Route path='login' element={<PublicRoutes user={user} />}>
-                            <Route path='' element={<Login />} />
+                        <Route path='/' element={<Landing />} />
+                        <Route path='public' element={<PublicRoutes user={user} />}>
+                            <Route path='public/login' element={<Login />} />
+                            <Route path='public/signup' element={<SignUp />} />
+                            <Route path='public/about-us' element={<Aboutus />} />
+                            <Route path='*' element={<Navigate to='/' />} />
                         </Route>
-                        <Route path='/' element={<PrivateRoutes user={user} />}>
-                            <Route path='home' element={<Home />}>
-                                {/* Default */}
-                            </Route>
+                        <Route path='private' element={<PrivateRoutes user={user} />}>
+                            <Route path='private/chat' element={<Chat />} />
+                            <Route path='private/edit-profile' element={<EditProfile />} />
+                            <Route path='private/profile' element={<Profile />} />
+                            <Route path='private/my-events' element={<MyEvents />} />
+                            <Route path='private/search' element={<Search />} />
+                            <Route path='private/create-events' element={<CreateEvents />} />
+                            <Route path='private//' element={<Events />} />
                             <Route path='*' element={<Navigate to='home' />} />
                         </Route>
                     </Routes>
